03a806bd1556a83ed7f6b9d30641e2335f875315
Novel-OxO <jungmini0601@gmail.com>
2026-02-15 16:14:25 +0900

    refactor(web): 메인 레이아웃에 ResizableLayout 적용
    - 고정 너비 사이드바에서 리사이즈 가능한 레이아웃으로 변경
    - ResizableLayout 컴포넌트를 사용하여 사이드바와 메인 콘텐츠 영역 분리

--- packages/web/src/app/(main)/layout.tsx
+++ packages/web/src/app/(main)/layout.tsx
@@ -1,10 +1,12 @@
+import { ResizableLayout } from '@/components/layout/resizable-layout';
 import { Sidebar } from '@/components/sidebar';
 
 export default function MainLayout({ children }: { children: React.ReactNode }): React.JSX.Element {
   return (
-    <div className="flex min-h-screen">
-      <Sidebar />
-      <main className="ml-60 flex-1">{children}</main>
+    <div className="min-h-screen h-screen">
+      <ResizableLayout leftSidebar={<Sidebar />} showLeftSidebar={true}>
+        {children}
+      </ResizableLayout>
     </div>
   );
 }
